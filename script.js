5ba688488d714cb27dd58e39d3fbf3f566d2c142
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-10 00:33:18 +0700

    Create function to determine human input and corresponding log.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let compChoice;
+
 function getComputerChoice(compChoice) {
     let random = (Math.random() * 100) + 1;
     if (random <= 33.333) {
@@ -8,6 +9,25 @@ function getComputerChoice(compChoice) {
     } else {
         compChoice = 'paper';
     }
-console.log(compChoice)
 }
 getComputerChoice();
+
+
+
+let humanChoice = 'rock';
+function getHumanChoice(humanChoice) {
+    let input = prompt('Choose wisely! Rock, paper or scissors...', 'Rock, Paper, Scissors (you can type: R, P or S)');
+    if (input === 'rock' || input === 'Rock' || input === 'r' || input === 'R') {
+        humanChoice = 'ROCK';
+        console.log(`Your choice is: ${humanChoice}`);
+    } else if (input === 'paper' || input === 'Paper' || input === 'p' || input === 'P') {
+        humanChoice = 'PAPER';
+        console.log(`Your choice is: ${humanChoice}`);
+    } else if (input === 'scissors' || input === 'Scissors' || input === 's' || input === 'S') {
+        humanChoice = 'SCISSORS';
+        console.log(`Your choice is: ${humanChoice}`);
+    } else {
+        console.log('You scared to loose? Put in the right text and face me!');
+    }
+}
+getHumanChoice();
