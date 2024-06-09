37bcea60ebfe524a391b7148b969e264c5783e23
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-09 23:28:44 +0700

    Create function to determine computer random choice.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-S
+let compChoice;
+function getComputerChoice(compChoice) {
+    let random = (Math.random() * 100) + 1;
+    if (random <= 33.333) {
+        compChoice = 'rock';
+    } else if (random > 33.333 && random <= 66.666) {
+        compChoice = 'scissors';
+    } else {
+        compChoice = 'paper';
+    }
+console.log(compChoice)
+}
+getComputerChoice();
