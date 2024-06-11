14bdd06fc5837a7dfb8894f4b487cb57a4a03f03
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-11 18:21:44 +0700

    Work for 1 round then can\'t return correct answer and as result can\'t add scores. Need to rework logic and variables.

--- script.js
+++ script.js
@@ -1,83 +1,157 @@
-function getCompChoice(compChoice) {
-    let random = (Math.random() * 100) + 1;
-    if (random <= 33.333) {
-        compChoice = 'ROCK';
-        return (compChoice);
-    } else if (random > 33.333 && random <= 66.666) {
-        compChoice = 'SCISSORS';
-        return (compChoice);
-    } else if (random > 66.666 && random <= 100) {
-        compChoice = 'PAPER';
-        return (compChoice);
+const rock = 'ROCK';
+const paper = 'PAPER';
+const scissors = 'SCISSORS';
+
+const win = 'You won!';
+const lost = 'You lost!';
+const draw = 'That\'s a draw!';
+
+const winAgain = 'You got lucky this time... Let\'s go again?';
+const lostAgain = 'You gave it a good try... Let\'s go again?';
+
+
+console.log('---------- Round 1 ----------')
+const inputHuman = getHumanChoice();
+const inputComputer = getComputerChoice();
+const inputGameLogic = getGameLogic();
+const inputScoreHuman = getScoreHuman();
+const inputScoreComputer = getScoreComputer();
+
+const newInputGame = newGame;
+const newInputHuman = getHumanChoice;
+const newInputComputer = getComputerChoice;
+// const newInputGameLogic = getGameLogic;
+const newInputScoreHuman = getScoreHuman;
+const newInputScoreComputer = getScoreComputer;
+
+let choiceComputer;
+let choiceHuman;
+let result;
+let newScoreHuman;
+let newScoreComputer;
+
+
+function getComputerChoice(choiceComputer) {
+    let value = (Math.random() * 100) + 1;
+    if (value < 33.33) {
+        choiceComputer = rock;
+    } else if (value >= 33.33 && value < 66.66) {
+        choiceComputer = paper;
+    } else if (value >= 66.66) {
+        choiceComputer = scissors;
     }
+    console.groupCollapsed('Computer: ', choiceComputer);
+    console.log('Computer choice value: ', value);
+    console.log('Computer choice: ', choiceComputer);
+    console.groupEnd();
+    return (choiceComputer)
 }
 
-function getHumanChoice(humanChoice) {
-    let input = prompt('Choose wisely! Rock, paper or scissors...', 'Rock, Paper, Scissors (you can type: R, P or S)');
-    if (input === 'rock' || input === 'Rock' || input === 'r' || input === 'R') {
-        humanChoice = 'ROCK';
-        // console.log(`Your choice is: ${humanChoice}`);
-        return (humanChoice);
-    } else if (input === 'paper' || input === 'Paper' || input === 'p' || input === 'P') {
-        humanChoice = 'PAPER';
-        // console.log(`Your choice is: ${humanChoice}`);
-        return (humanChoice);
-    } else if (input === 'scissors' || input === 'Scissors' || input === 's' || input === 'S') {
-        humanChoice = 'SCISSORS';
-        // console.log(`Your choice is: ${humanChoice}`);
-        return (humanChoice);
+function getHumanChoice(choiceHuman) {
+    let input = prompt('Rock, paper, scissors... shooot!', '');
+    let inputNoCase = input.toLowerCase();
+
+    if (inputNoCase === 'rock' || inputNoCase === 'r') {
+        choiceHuman = rock;
+    } else if (inputNoCase === 'paper' || inputNoCase === 'p') {
+        choiceHuman = paper;
+    } else if (inputNoCase === 'scissors' || inputNoCase === 's') {
+        choiceHuman = scissors;
     } else {
-        console.log('You scared to loose? Put in the right text and let\'s do this!');
-    }
+        console.log('Till next time!');
+        return;
+    };
+    console.groupCollapsed('You: ', choiceHuman);
+    console.log('Your input: ', input);
+    console.log('Your choice: ', choiceHuman);
+    console.groupEnd();
+    return (choiceHuman);
 }
 
-const humanChoice = getHumanChoice();
-console.log('Your choice: ', humanChoice);
-const compChoice = getCompChoice();
-console.log('Computer choice: ', compChoice);
-
-let compScore = 0;
-let humanScore = 0;
-
-function playRound(humanChoice, compChoice) {
-    let result = 'lost';
-    if ((compChoice === 'ROCK' && humanChoice === 'SCISSORS') || (humanChoice === 'SCISSORS' && compChoice === 'ROCK')) {
-        humanScore++;
-        console.log('You won!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-    } else if ((compChoice === 'PAPER' && humanChoice === 'ROCK') || (humanChoice === 'ROCK' && compChoice === 'PAPER')) {
-        humanScore++;
-        console.log('You won!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-    } else if ((compChoice === 'SCISSORS' && humanChoice === 'PAPER') || (humanChoice === 'PAPER' && compChoice === 'SCISSORS')) {
-        humanScore++;
-        console.log('You won!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-    } else if ((compChoice === 'ROCK' && humanChoice === 'PAPER') || (humanChoice === 'PAPER' && compChoice === 'ROCK')) {
-        compScore++;
-        console.log('You lost!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-    } else if ((compChoice === 'PAPER' && humanChoice === 'SCISSORS') || (humanChoice === 'SCISSORS' && compChoice === 'PAPER')) {
-        compScore++;
-        console.log('You lost!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-    } else if ((compChoice === 'SCISSORS' && humanChoice === 'ROCK') || (humanChoice === 'ROCK' && compChoice === 'SCISSORS')) {
-        compScore++;
-        console.log('You lost!');
-        console.log('Your score: ', humanScore)
-        console.log('Computer score: ', compScore)
-        } else if (compChoice === humanChoice) {
-            humanScore++;
-            compScore++;
-            console.log('That\'s a draw! What are the chances of that huh?! 33.33% actually ;) ')
-            console.log('Your score: ', humanScore)
-            console.log('Computer score: ', compScore)
+function getGameLogic(result) {
+    if ((inputHuman === rock && inputComputer === scissors) || (inputComputer === scissors && inputHuman === rock)) {
+        result = win;
+        // setTimeout(getGameLogic, 1000);
+    } else if ((inputHuman === paper && inputComputer === rock) || (inputComputer === rock && inputHuman === paper)) {
+        result = win;
+        // setTimeout(getGameLogic, 1000);
+    } else if ((inputHuman === scissors && inputComputer === paper) || (inputComputer === paper && inputHuman === scissors)) {
+        result = win;
+        // setTimeout(getGameLogic, 1000);
+    } else if ((inputHuman === inputComputer) || (inputComputer === inputHuman)) {
+        result = draw;
+        // setTimeout(getGameLogic, 1000);
+    } else {
+        result = lost;
+        // setTimeout(getGameLogic, 1000);
     }
+
+    console.groupCollapsed(result);
+    console.log('Result: ', result);
+    console.log('Human input: ', inputHuman);
+    console.log('Computer input: ', inputComputer);
+    console.groupEnd();
+    // setTimeout(getGameLogic, 1000);
+    // clearTimeout(getGameLogic);
+    // setTimeout(getGameLogic, 10);
+    // getGameLogic();
+    return (result);
 }
 
-playRound(compChoice, humanChoice);
+
+function getScoreHuman(newScoreHuman) {
+    let scoreHuman = 0;
+
+    if (inputGameLogic === win) {
+        newScoreHuman = ++scoreHuman;
+    } else if (inputGameLogic === draw) {
+        newScoreHuman = ++scoreHuman;
+    } else {
+        scoreHuman;
+    }
+
+    console.groupCollapsed('Your score: ', scoreHuman);
+    console.log('Your score: ', newScoreHuman);
+    console.log('Result: ', inputGameLogic);
+    console.groupEnd();
+    return (newScoreHuman);
+}
+
+function getScoreComputer(newScoreComputer) {
+    let scoreComputer = 0;
+
+    if (inputGameLogic === lost) {
+        newScoreComputer = ++scoreComputer;
+    } else if (inputGameLogic === draw) {
+        newScoreComputer = ++scoreComputer;
+    } else {
+        scoreComputer;
+    }
+
+    console.groupCollapsed('Comupter score: ', scoreComputer);
+    console.log('Computer score: ', newScoreComputer);
+    console.log('Result: ', inputGameLogic);
+    console.groupEnd();
+    return (newScoreComputer);
+}
+
+function newGame() {
+    let restart = 'true';
+    if (restart === 'true') {
+        restart = confirm('Play another game?', '');
+        if (restart) {
+            console.log('---------- Round 2 ----------');
+            getHumanChoice();
+            getComputerChoice();
+            getGameLogic();
+            newInputScoreHuman();
+            newInputScoreComputer();
+        } else {
+            console.log('Another time then.')
+
+        }
+        console.log(restart);
+        return;
+    }
+}
+setTimeout(newGame, 1500);
