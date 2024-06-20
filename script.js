ebdf61b1091bb05ad8ea94f24fc9713e5b8b3ebc
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-06-20 18:29:41 +0700

    Work on script in JS and little work in HTML. Button functionality now works and textContent added.

--- script.js
+++ script.js
@@ -1,185 +1,68 @@
+const btnArray = document.querySelectorAll('button');
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
+
+const textGame = document.querySelector('.container-game-text');
+const hChoiceText = document.querySelector('.choice-human');
+const cChoiceText = document.querySelector('.choice-computer');
+const gResult = document.querySelector('.result');
+
 const rock = 'ROCK';
 const paper = 'PAPER';
 const scissors = 'SCISSORS';
 
-const win = 'You won!';
-const lost = 'You lost!';
-const draw = 'That\'s a draw!';
+const win = 'YOU WON !';
+const lost = 'YOU LOST !';
+const draw = 'THAT\'S A DRAW !';
 
-const setRound = 3;
+const cChoice = getComputerChoice();
+console.log('computer choice: ', cChoice);
+cChoiceText.textContent = `Computer choice: ${cChoice}`
 
-console.warn('|------------ ROUND 1 ------------|')
+btnArray.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const hChoice = getHumanChoice(e);
+        const gameResult = getGame(hChoice, cChoice);
+        console.log('your choice: ', hChoice)
+        console.log('results: ', gameResult);
+        hChoiceText.textContent = `You\'re choice: ${hChoice}`;
+        gResult.textContent = gameResult;
+    })
+})
 
-let humanScore = 0;
-let computerScore = 0;
-let roundAmount = 1;
+function getHumanChoice(e) {
+    let humanChoice;
+    if (e.target === btnRock) {
+        humanChoice = rock;
+    } else if (e.target === btnPaper) {
+        humanChoice = paper;
+    } else if (e.target === btnScissors) {
+        humanChoice = scissors
+    }
+    return humanChoice;
+}
 
 function getComputerChoice() {
-    let value = (Math.random() * 100) + 1;
-    let computerChoice;
-
-    if (value < 33.33) {
+    let computerChoice = ((Math.random()) * 100).toFixed(0);
+    if (computerChoice <= 33) {
         computerChoice = rock;
-    } else if (value >= 33.33 && value < 66.66) {
+    } else if (computerChoice > 33 && computerChoice <= 66) {
         computerChoice = paper;
-    } else if (value > 66.66) {
+    } else {
         computerChoice = scissors;
     }
-
-    console.groupCollapsed('Computer choice: ', computerChoice);
-    console.log('Computer choice value: ', value);
-    console.groupEnd();
-    return (computerChoice);
+    return computerChoice
 }
 
-function getHumanChoice() {
-    let input = prompt('Rock, paper, scissors... SHOOT!', '');
-    let inputNoCase;
-    let humanChoice;
-
-    if (input === null) {
-        inputNoCase = input;
+function getGame(hChoice, cChoice) {
+    let result = '';
+    if (hChoice === rock && cChoice === scissors || hChoice === paper && cChoice === rock || hChoice === scissors && cChoice === rock) {
+        result = win;
+    } else if (hChoice === cChoice) {
+        result = draw;
     } else {
-        inputNoCase = input.toLowerCase();
+        result = lost;
     }
-
-    if (inputNoCase === 'rock' || inputNoCase === 'r') {
-        humanChoice = rock;
-        console.groupCollapsed('Your choice: ', humanChoice);
-        console.log('Your choice input: ', input);
-        console.groupEnd();
-        return (humanChoice);
-    } else if (inputNoCase === 'paper' || inputNoCase === 'p') {
-        humanChoice = paper;
-        console.groupCollapsed('Your choice: ', humanChoice);
-        console.log('Your choice input: ', input);
-        console.groupEnd();
-        return (humanChoice);
-    } else if (inputNoCase === 'scissors' || inputNoCase === 's') {
-        humanChoice = scissors;
-        console.groupCollapsed('Your choice: ', humanChoice);
-        console.log('Your choice input: ', input);
-        console.groupEnd();
-        return (humanChoice);
-    } else {
-        if (confirm('Are you sure you want to leave?')) {
-            console.log('Till next time.');
-            return null;
-        } else {
-            playGame();
-        }
-    }
+    return result;
 }
-
-function playRound(humanChoice, computerChoice) {
-    let gameResult;
-    let backgroundColor;
-
-    if ((humanChoice === rock && computerChoice === scissors) || (humanChoice === paper && computerChoice === rock) || (humanChoice === scissors && computerChoice === paper)) {
-        gameResult = win;
-        backgroundColor = 'background-color: green';
-    } else if (humanChoice === computerChoice) {
-        gameResult = draw;
-        backgroundColor = 'background-color: blue';
-    } else {
-        gameResult = lost;
-        backgroundColor = 'background-color: red';
-    }
-
-    console.groupCollapsed(`%c${gameResult}`, backgroundColor);
-    console.log('Your choice: ', humanChoice);
-    console.log('Computer choice: ', computerChoice);
-    if (gameResult === win) {
-        console.log(`${humanChoice} beats ${computerChoice}`);
-    } else if (gameResult === draw) {
-        console.log(`${computerChoice} draw ${humanChoice}`);
-    } else {
-        console.log(`${computerChoice} beats ${humanChoice}`);
-    }
-    console.groupEnd();
-    return (gameResult);
-}
-
-function keepScore(gameResult) {
-    let whoInLead;
-    let backgroundColor;
-
-    if (gameResult === win) {
-        ++humanScore;
-        ++roundAmount;
-    } else if (gameResult === lost) {
-        ++computerScore;
-        ++roundAmount;
-    } else if (gameResult === draw) {
-        ++roundAmount;
-    }
-
-    if (humanScore === setRound) {
-        whoInLead = 'Well done, you WON';
-        backgroundColor = 'background-color: darkgreen'
-    } else if (computerScore === setRound) {
-        whoInLead = 'You LOST, better luck next time';
-        backgroundColor = 'background-color: darkred'
-    } else if (humanScore > computerScore) {
-        whoInLead = 'You\'re winning';
-        backgroundColor = 'background-color: green'
-    } else if (humanScore < computerScore) {
-        whoInLead = 'You\'re loosing';
-        backgroundColor = 'background-color: red'
-    } else if (humanScore === computerScore) {
-        whoInLead = 'You\'re neck and neck';
-        backgroundColor = 'background-color: blue'
-    }
-
-    console.groupCollapsed(`%cYour score: ${humanScore} vs Computer score: ${computerScore}. ${whoInLead}!`, backgroundColor);
-    console.log('Human score: ', humanScore);
-    console.log('Computer score: ', computerScore);
-    console.log('Result: ', gameResult);
-    console.groupEnd();
-
-    if (computerScore === setRound || humanScore === setRound) {
-        console.warn('|------------ END GAME ------------|');
-    }
-}
-
-function playGame() {
-    const humanChoice = getHumanChoice();
-
-    if (humanChoice === rock || humanChoice === paper || humanChoice === scissors) {
-        const computerChoice = getComputerChoice();
-        const gameResult = playRound(humanChoice, computerChoice);
-        keepScore(gameResult);
-
-        if (humanScore === setRound || computerScore === setRound) {
-            setTimeout(restartGame(), 5000);
-        } else if (humanScore < setRound && computerScore < setRound) {
-            console.warn(`|------------ ROUND ${roundAmount} ------------|`)
-            playGame();
-        }
-    }
-}
-
-function startGame() {
-    if (confirm(`Rock, Paper, Scissors? First to ${setRound} wins?`)) {
-        playGame();
-    } else {
-        console.clear();
-        console.log('Till next time.');
-    }
-}
-
-function restartGame() {
-    if (confirm('Another game?')) {
-        humanScore = humanScore * 0;
-        computerScore = computerScore * 0;
-        roundAmount = (roundAmount * 0) + 1;
-        console.warn('|------------ ROUND 1 ------------|')
-        playGame();
-    } else if (active = 'true') {
-        console.log('Till next Time.');
-    }
-}
-
-startGame();
-
-console.warn('|------------ END GAME ------------|')
